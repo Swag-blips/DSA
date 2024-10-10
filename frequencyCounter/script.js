b4bce8d621603b8f346813ec98cd8b5946dce574
Swag-blips <coderblip@gmail.com>
2024-10-10 01:09:57 +0100

    multiple pointers changes

--- frequencyCounter/script.js
+++ frequencyCounter/script.js
@@ -121,8 +121,36 @@ const sameFrequency = (num1, num2) => {
   }
 
   return true;
-
-  
 };
 
 console.log(sameFrequency(22, 222));
+
+// Frequency Counter / Multiple Pointers -
+// // areThereDuplicates Implement a function called,
+// // areThereDuplicates which accepts a variable number of
+// arguments, and checks whether there are any duplicates
+// among the arguments passed in.  You can solve this
+// using the frequency counter pattern OR the multiple
+// pointers pattern.
+
+// Examples:
+
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a')
+
+const areThereDuplicates = (...arg) => {
+  const counter = {};
+
+  for (const arr of arg) {
+    counter[arr] = (counter[arr] || 0) + 1;
+  }
+
+  for (const key in counter) {
+    if (counter[key] > 1) return true;
+  }
+
+  return false;
+};
+
+console.log(areThereDuplicates(1, 2, 2));
--- multiple pointers/script.js
+++ multiple pointers/script.js
@@ -92,3 +92,19 @@ const twoNum = (arr) => {
 };
 
 console.log(twoNum([2, 7, 11, 15]));
+
+// Frequency Counter / Multiple Pointers -
+// // areThereDuplicates Implement a function called,
+// // areThereDuplicates which accepts a variable number of
+// arguments, and checks whether there are any duplicates
+// among the arguments passed in.  You can solve this
+// using the frequency counter pattern OR the multiple
+// pointers pattern.
+
+// Examples:
+
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a')
+
+// bruteforce
