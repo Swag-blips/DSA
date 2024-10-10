2a1a5bf1abdf615c8c83f3cba7799ad7c93faa7b
Swag-blips <coderblip@gmail.com>
2024-10-10 09:28:59 +0100

    changes

--- multiple pointers/script.js
+++ multiple pointers/script.js
@@ -108,3 +108,52 @@ console.log(twoNum([2, 7, 11, 15]));
 // areThereDuplicates('a', 'b', 'c', 'a')
 
 // bruteforce
+
+const areThereDuplicates = (...arg) => {
+  for (let i = 0; i < arg.length; i++) {
+    for (let j = i + 1; j < arg.length; j++) {
+      if (arg[i] == arg[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+console.log(areThereDuplicates("a", "b", "c", "a"));
+
+// Multiple Pointers - averagePair
+// Write a function called averagePair. Given a sorted array of integers and a target average,
+// determine if there is a pair of values in the array where the average of the pair equals the target average.
+// There may be more than one pair that matches the average target.
+
+// Bonus Constraints:
+
+// Time: O(N)
+
+// Space: O(1)
+
+// Sample Input:
+
+// averagePair([1,2,3],2.5) // true
+// averagePair([1,3,3,5,6,7,10,12,19],8) // true
+// averagePair([-1,0,3,4,5,6], 4.1) // false
+// averagePair([],4) // false
+
+const averagePair = (arr, target) => {
+  let i = 0;
+  let j = arr.length - 1;
+  while (i < j) {
+    const avg = (arr[i] + arr[j]) / 2;
+    if (avg === target) {
+      return true;
+    } else if (avg < target) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+  return false;
+};
+
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
