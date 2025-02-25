b2af95fbf95af1ca1d1cacfba300760a794c160e
Swag-blips <coderblip@gmail.com>
2025-02-25 07:54:11 +0100

    feat:Array shift method

--- HuXn/Arrays.js
+++ HuXn/Arrays.js
@@ -12,11 +12,32 @@ class CustomArray {
   get(index) {
     return this.data[index];
   }
+
+  pop() {
+    delete this.data[this.length - 1];
+    this.length--;
+    return;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    const firstElement = this.data[0];
+
+    for (let i = 0; i < this.length; i++) {
+      this.data[i - 1] = this.data[i];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
 
 const myArray = new CustomArray();
 
-myArray.push(4);
-myArray.push(6);
-console.log(myArray.get(1));
+myArray.push("salad");
+myArray.push("fish");
+myArray.push("mango");
+myArray.shift();
+
 console.log(myArray);
