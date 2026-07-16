6d417d3a4c2d7159afc08d24df2f6ebe5ca9729e
Bakrin Olasupo <coderblip@gmail.com>
2026-07-16 20:36:05 +0100

    feat:evaluate reverse polish notation

--- stack/problems/evaluateReversePolishNotation.js
+++ stack/problems/evaluateReversePolishNotation.js
@@ -25,3 +25,75 @@ class Stack {
     return this.stack.length;
   }
 }
+
+const stack = new Stack();
+
+function evaluateReversePolishNotation(tokens) {
+  for (let i = 0; i < tokens.length; i++) {
+    const currentToken = tokens[i];
+
+    if (
+      currentToken !== "+" &&
+      currentToken !== "-" &&
+      currentToken !== "/" &&
+      currentToken !== "*"
+    ) {
+      stack.push(currentToken);
+      continue;
+    }
+
+    if (currentToken === "+") {
+      const previousDigit = Number(stack.peek(2));
+      const previousTwoDigit = Number(stack.peek(1));
+
+      let newDigit = previousDigit + previousTwoDigit;
+      stack.pop();
+      stack.pop();
+      stack.push(newDigit);
+    } else if (currentToken === "-") {
+      const previousDigit = Number(stack.peek(1));
+      const previousTwoDigit = Number(stack.peek(2));
+
+      let newDigit = previousDigit - previousTwoDigit;
+      stack.pop();
+      stack.pop();
+      stack.push(newDigit);
+    } else if (currentToken === "/") {
+      const previousDigit = Number(stack.peek(2));
+      const previousTwoDigit = Number(stack.peek(1));
+
+      let newDigit = Math.trunc(previousDigit / previousTwoDigit);
+      stack.pop();
+      stack.pop();
+      stack.push(newDigit);
+    } else {
+      const previousDigit = Number(stack.peek(2));
+      const previousTwoDigit = Number(stack.peek(1));
+
+      let newDigit = previousDigit * previousTwoDigit;
+      stack.pop();
+      stack.pop();
+      stack.push(newDigit);
+    }
+  }
+
+  return stack.stack.join("");
+}
+
+console.log(
+  evaluateReversePolishNotation([
+    "10",
+    "6",
+    "9",
+    "3",
+    "+",
+    "-11",
+    "*",
+    "/",
+    "*",
+    "17",
+    "+",
+    "5",
+    "+",
+  ]),
+);
